51fc3eb976061200f0e90aaf21ac9b0c7ca6f033
Z User <z@container>
2026-06-29 06:52:31 +0000

    FIX VERCEL: remove ALL build artifacts from git tracking
    Removed: .vercel/, .open-next/, db/, upload/, tool-results/, android/, dist/, dev.log
    These were committed before .gitignore was updated and caused Vercel to
    try to use stale node_modules/.vercel output instead of fresh install.

--- .vercel/output/static/_worker.js/__next-on-pages-dist__/manifest/__RSC_SERVER_MANIFEST.js
+++
@@ -1,5 +0,0 @@
-var e={},s=(_,n,o)=>(e.__RSC_SERVER_MANIFEST=_.__RSC_SERVER_MANIFEST=`{
-  "node": {},
-  "edge": {},
-  "encryptionKey": "wAWRhv3ds6amY/BiZQdOx5QgHJ/Ams3QscIsp/SbeTg="
-}`,e);export{s as __getNamedExports};
--- android/app/src/main/java/com/zexbox/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.zexbox.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
