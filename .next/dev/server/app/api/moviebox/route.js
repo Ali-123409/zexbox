064cef5bfeac7114925d4111c5d6eb269c036ffb
Z User <z@container>
2026-06-29 06:12:32 +0000

    2037be8d-d44c-4ee0-97c8-f6ebe2459ae0

---
+++ .next/dev/server/app/api/moviebox/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/moviebox/route.js")
+R.c("server/chunks/node_modules_next_59f5ae52._.js")
+R.c("server/chunks/[root-of-the-server]__68f28aa1._.js")
+R.c("server/chunks/_next-internal_server_app_api_moviebox_route_actions_853c2620.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/moviebox/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/moviebox/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
---
+++ .next/dev/server/chunks/_next-internal_server_app_api_moviebox_route_actions_853c2620.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/.next-internal/server/app/api/moviebox/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=_next-internal_server_app_api_moviebox_route_actions_853c2620.js.map
